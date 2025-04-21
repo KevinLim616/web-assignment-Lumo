9e0f526d1b7f22e22e394002c06a1f25efe38ba8
KevinLim616 <kevinlim198@gmail.com>
2025-04-21 08:57:10 +0800

    successfully check email existence & prevent sql injection

--- authentication/login_functions.php
+++ authentication/login_functions.php
@@ -6,14 +6,21 @@ include __DIR__ . "/../include/db/database.php";
 
 function userExist($email, $password)
 {
-    // select from account table where username and password match
-    $sql = "SELECT * FROM account WHERE email = '$email' AND password = '$password'";
     global $conn;
-    $result = mysqli_query($conn, $sql);
+    // select from account table where username and password match
+    $sql = "SELECT * FROM account WHERE email = ? ";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
 
     //if there are results
-    if (mysqli_num_rows($result) > 0) {
-        return true;
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            return true;
+        }
     } else {
         //if there are no results
         return false;
@@ -24,8 +31,7 @@ function userExist($email, $password)
 function login($email, $password)
 {
     //sanitize input
-    $email = htmlspecialchars($email);
-    $password = htmlspecialchars($password);
+    $email = filter_var(trim($email), FILTER_SANITIZE_EMAIL);
     //filter input
 
     // Check if the email and password are correct
@@ -33,7 +39,7 @@ function login($email, $password)
     if ($email == "admin@example.com" && $password == "password") {
         //redirect to admin page
         header("Location: admin/admin.php");
-    } else if (userExist($email, $password) == true) { //search existed user in db
+    } else if (userExist($email, $password)) { //search existed user in db
         echo "user logged in";
     } else {
         echo "Invalid email or password.";
