5b6db2a18b77fdec531f13c61d850a9d80b13943
KevinLim616 <kevinlim198@gmail.com>
2025-05-06 19:43:38 +0800

    change the sql code to get missing data (description) for the drawer

--- tasks/get_tasks.php
+++ tasks/get_tasks.php
@@ -4,8 +4,14 @@ include __DIR__ . "./../include/db/database.php";
 function getTasks()
 {
     global $conn;
-    $sql = "SELECT id, title, time, category FROM task WHERE status ='pending' ORDER BY date, time";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    try {
+        global $conn;
+        $sql = "SELECT id, title,date, time,description, category FROM task WHERE status ='pending' ORDER BY date, time";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $error) {
+        error_log("Error fetching tasks:" . $error->getMessage());
+        return [];
+    }
 }
